fd086d738bff9f3cc11f1b784936f395d3569cb1
Jheimys Bassani <jheimysbb@gmail.com>
2022-06-28 15:25:01 -0300

    Autenticacao de usuario

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -7,6 +7,34 @@ module.exports = class AuthController {
         res.render('auth/login')
     }
 
+    static async loginPost(req, res) {
+        const { email, password } = req.body
+
+        //verificação do email
+        const user = await User.findOne({ where: { email: email } })
+
+        if(!user) {
+            req.flash('message', 'Usuário não encontrado')
+            res.render('auth/login')
+        }
+
+        //verificação da senha
+        const passwordMatch = bcrypt.compareSync(password, user.password)
+
+        if(!passwordMatch) {
+            req.flash('message', 'Senha inválida')
+            res.render('auth/login')
+        }
+
+        //Inicializando a sessão
+        req.session.userid = user.id
+
+        req.flash('message', 'Autenticação realizada com sucesso!')
+        req.session.save(() => {
+            res.redirect('/')
+        })
+    }
+
     static register(req, res) {
         res.render('auth/register')
     }
