e231c81875d8d3a2573cf69b59b1511df5ea6e9a
Jheimys Bassani <jheimysbb@gmail.com>
2022-06-28 16:07:36 -0300

    verificacao da atualizacao

--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -5,6 +5,10 @@ const router = express.Router()
 //controllers
 const ToughtController = require('../controllers/ToughtController')
 
+//helpers
+const checkAuth = require('../helpers/auth').checkAuth
+
+router.get('/dashboard',checkAuth, ToughtController.dashboard)
 router.get('/', ToughtController.showToughts)
 
 module.exports = router
